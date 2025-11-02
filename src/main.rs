c63a35971b8b63d71be7f46a238b017fae0cbb64
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-01 23:35:39 -0400

    First real version!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use xilem::masonry::properties::types::Length;
+use xilem::style::{Padding, Style};
+use xilem::view::{
+    VirtualScroll, canvas, flex_col, sized_box, unlimited_virtual_scroll, virtual_scroll,
+};
 use xilem::winit::error::EventLoopError;
 use xilem::{EventLoop, EventLoopBuilder, WidgetView, WindowOptions, Xilem};
 
@@ -18,16 +23,26 @@ fn run(event_loop: EventLoopBuilder) -> Result<(), EventLoopError> {
         app,
         WindowOptions::new("A PDF Application"),
     )
-    .run_in(event_loop);
-
-    Ok(())
+    .run_in(event_loop)
 }
 
 fn app(state: &mut MainState) -> impl WidgetView<MainState> + use<> {
-    let scene = vello_svg::render(r###""###).unwrap();
-    let canvas = canvas(move |scened, size| {
-        *scened = scene.clone();
-    });
+    let svg1 = include_str!("/Users/philocalyst/Downloads/hayro/hayro-svg/rendered_0.svg");
+    let svg2 = include_str!("/Users/philocalyst/Downloads/hayro/hayro-svg/examples/rendered_0.svg");
 
-    canvas
+    virtual_scroll(0..1, move |state, index| {
+        flex_col((
+            sized_box(canvas(move |scened, _| {
+                let scene = vello_svg::render(svg2).unwrap();
+                *scened = scene;
+            }))
+            .height(Length::px(300.0)),
+            sized_box(canvas(move |scened, _| {
+                let scene = vello_svg::render(svg1).unwrap();
+                *scened = scene;
+            }))
+            .height(Length::px(300.0)),
+        ))
+        .padding(Padding::from(200.0))
+    })
 }
