a69588a79cf1d62e73814a2b785506b4794efe8d
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-02 00:13:34 -0400

    Attempting to lock down the height aspect

--- src/main.rs
+++ src/main.rs
@@ -26,26 +26,23 @@ fn run(event_loop: EventLoopBuilder) -> Result<(), EventLoopError> {
     .run_in(event_loop)
 }
 
-fn render_svg_to_canvas(
-    svg_str: &'static str,
-    target_height: f64,
-) -> impl WidgetView<MainState> {
-    sized_box(canvas(move |scene, size| {
-        // Parse SVG and generate Vello scene
-        let svg_scene = vello_svg::render(svg_str).unwrap();
-        let tree = usvg::Tree::from_str(svg_str, &usvg::Options::default()).unwrap();
-        let svg_size = tree.size();
+fn render_svg_to_canvas(svg_str: &'static str) -> impl WidgetView<MainState> {
+    // Parse SVG and generate Vello scene
+    let svg_scene = vello_svg::render(svg_str).unwrap();
+    let tree = usvg::Tree::from_str(svg_str, &usvg::Options::default()).unwrap();
+    let svg_size = tree.size();
 
+    sized_box(canvas(move |scene, size| {
         // Compute scaling
         let scale_x = size.width / svg_size.width() as f64;
 
-        let transform = 
-            // Uniform scaling about SVG center
+        // Uniform scaling about SVG center
+        let transform =
             Affine::scale_about(scale_x, (svg_size.width() / 2.0, svg_size.height() / 2.0));
 
         scene.append(&svg_scene, Some(transform));
     }))
-    .height(Length::px(target_height))
+    .height(Length::px(svg_size.height() as f64))
 }
 
 fn app(state: &mut MainState) -> impl WidgetView<MainState> + use<> {
@@ -53,10 +50,7 @@ fn app(state: &mut MainState) -> impl WidgetView<MainState> + use<> {
     let svg2 = include_str!("/Users/philocalyst/Downloads/hayro/hayro-svg/examples/rendered_0.svg");
 
     virtual_scroll(0..1, move |state, index| {
-        flex_col((
-            render_svg_to_canvas(svg2, 200.0),
-            render_svg_to_canvas(svg1, 400.0),
-        ))
-        .padding(Padding::from(200.0))
+        flex_col((render_svg_to_canvas(svg2), render_svg_to_canvas(svg1)))
+            .padding(Padding::from(200.0))
     })
 }
