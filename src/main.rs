0ac0b811ca3e2e17f7af2a504d092047b9b4eee7
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-01 23:05:41 -0400

    App scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
-use xilem::EventLoop;
 use xilem::winit::error::EventLoopError;
+use xilem::{EventLoop, EventLoopBuilder, WidgetView, WindowOptions, Xilem};
+
+enum MainState {
+    Online,
+}
 
 fn main() -> Result<(), EventLoopError> {
     println!("Hello, world!");
 
+    run(EventLoop::with_user_event());
     Ok(())
+}
+
+fn run(event_loop: EventLoopBuilder) -> Result<(), EventLoopError> {
+    Xilem::new_simple(
+        MainState::Online,
+        app,
+        WindowOptions::new("A PDF Application"),
+    )
+    .run_in(event_loop);
+
+    Ok(())
+}
+
+fn app(state: &mut MainState) -> impl WidgetView<MainState> + use<> {
+    let scene = vello_svg::render(r###""###).unwrap();
+    let canvas = canvas(move |scened, size| {
+        *scened = scene.clone();
+    });
+
+    canvas
 }
