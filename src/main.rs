e3a3fef995298756b8d72638f538755f8faa9e4a
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-27 23:17:36 -0500

    Updated to latest xilem version and added tracing support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use std::sync::Arc;
+use tracing::{debug, info};
+use xilem::core::Edit;
 use xilem::masonry::properties::types::Length;
-use xilem::style::{Padding, Style};
-use xilem::view::{
-    VirtualScroll, canvas, flex_col, sized_box, unlimited_virtual_scroll, virtual_scroll,
-};
+use xilem::view::{canvas, sized_box, virtual_scroll};
 use xilem::winit::error::EventLoopError;
 use xilem::{Affine, EventLoop, EventLoopBuilder, WidgetView, WindowOptions, Xilem};
 
@@ -11,8 +11,14 @@ enum MainState {
 }
 
 fn main() -> Result<(), EventLoopError> {
-    println!("Hello, world!");
-
+    // Initialize tracing
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing::Level::INFO.into()),
+        )
+        .init();
+    info!("Starting PDF application");
     run(EventLoop::with_user_event())
 }
 
@@ -25,30 +31,33 @@ fn run(event_loop: EventLoopBuilder) -> Result<(), EventLoopError> {
     .run_in(event_loop)
 }
 
-fn render_svg_to_canvas(svg_str: &'static str) -> impl WidgetView<MainState> {
+fn render_svg_to_canvas(svg_str: &'static str) -> impl WidgetView<Edit<MainState>> + use<> {
+    debug!("Rendering SVG to canvas");
     // Parse SVG and generate Vello scene
     let svg_scene = vello_svg::render(svg_str).unwrap();
     let tree = usvg::Tree::from_str(svg_str, &usvg::Options::default()).unwrap();
     let svg_size = tree.size();
+    debug!("SVG size: {}x{}", svg_size.width(), svg_size.height());
 
-    sized_box(canvas(move |scene, size| {
+    sized_box(canvas(Arc::new(move |scene, size| {
+        tracing::trace!("Canvas draw callback invoked with size: {:?}", size);
         // Compute scaling
         let scale_x = size.width / svg_size.width() as f64;
-
         // Uniform scaling about SVG center
         let transform =
             Affine::scale_about(scale_x, (svg_size.width() / 2.0, svg_size.height() / 2.0));
-
         scene.append(&svg_scene, Some(transform));
-    }))
-    .height(Length::px(svg_size.height() as f64))
+    })))
+    .height(Length::px((svg_size.height() + 400f32) as f64))
 }
 
-fn app(state: &mut MainState) -> impl WidgetView<MainState> + use<> {
-    let svg1 = include_str!("../testing_pdfs/rendered_1.svg");
+fn app(_state: &mut MainState) -> impl WidgetView<Edit<MainState>> + use<> {
+    info!("Building app view");
+    let svg1 = include_str!("../testing_pdfs/rendered_2.svg");
     let svg2 = include_str!("../testing_pdfs/rendered_0.svg");
 
-    virtual_scroll(0..2, move |state, index| {
+    virtual_scroll(0..2, move |_state: &mut MainState, index| {
+        debug!("Virtual scroll rendering item {}", index);
         let svg = match index {
             0 => svg1,
             1 => svg2,
